183aa01732c5e12da7580162d8673fb58627f915
tortumlu <ahmet.tortumlu@dofotech.com>
2020-11-24 22:55:41 +0300

    Add formik and material-ui to Signup Page

---
+++ src/pages/Signup.js
@@ -0,0 +1,76 @@
+import React from "react"
+import { Button, TextField, Grid, Container, useForkRef } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { useFormik } from 'formik';
+
+
+const styles = makeStyles(
+    {
+        wrapper: {
+            marginTop : "5rem",
+        }
+    }
+);
+
+function Signup(){
+    const formik = useFormik(
+        {
+            initialValues: {
+                displayName: "",
+                email: "",
+                password: ""
+            },
+            onSubmit: (values)=>{
+                alert(JSON.stringify(values, null, 2));
+            }
+        }
+    )
+    const signupStyles = styles()
+    return(
+        <Container className={signupStyles.wrapper} maxWidth="xm">
+            <form>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                <TextField name="displayName" label="Display Name" variant="outlined" 
+                fullWidth
+                value={formik.values.displayName}
+                onChange={formik.handleChange}/>
+                </Grid>
+
+                <Grid item xs={12}>
+                    <TextField
+                        name="email"
+                        label="Email"
+                        variant="outlined"
+                        fullWidth
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        name="password"
+                        label="Password"
+                        variant="outlined"
+                        fullWidth
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                />                </Grid>
+
+                <Grid item xs={12}>
+                <Button variant="contained"  color="primary" fullWidth >Submit</Button>
+                </Grid>
+
+                <Grid item xs={12}>
+                <Button variant="contained"  color="primary" fullWidth >Signup With Google</Button>
+                </Grid>
+                {
+
+                }
+            </Grid>
+            </form>
+        </Container>
+    )
+}
+
+export default Signup
